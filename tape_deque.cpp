5099150c5d0b4814004df185c5e2ffac03968da8
David Fontenot <davidmfontenot@gmail.com>
2014-10-25 10:11:39 -0700

    initial code import

---
+++ tape_deque.cpp
@@ -0,0 +1,59 @@
+#include "tape_deque.hpp"
+
+TapeDeque::TapeDeque()
+{
+    head = new node_t(false, NULL, NULL);
+    currentPosition = head;
+}
+
+TapeDeque::~TapeDeque() {}
+
+//return the score (or how many ones are written)
+int TapeDeque::score()
+{
+    //go all the way to the left and count upwards
+    node_t* current = head;
+    int score = 0;
+    
+    while(current->left != NULL)
+    {
+        current = current->left;
+    }
+    
+    while(current->right != NULL)
+    {
+        if(current->value)
+	    score++;
+	
+        current = current->right;
+    }
+    
+    return score;
+}
+
+void TapeDeque::moveRight(bool write)
+{
+    //create new node if true, else overwrite
+    if(currentPosition->right == NULL)
+        currentPosition->right = new node_t(write, currentPosition, NULL);
+    else
+        currentPosition->right->value = write;
+    
+    currentPosition = currentPosition->right;
+}
+
+void TapeDeque::moveLeft(bool write)
+{
+    //create new node if true, else overwrite
+    if(currentPosition->left == NULL)
+        currentPosition->left = new node_t(write, NULL, currentPosition);
+    else
+        currentPosition->left->value = write;
+    
+    currentPosition = currentPosition->left;
+}
+
+bool TapeDeque::currentValue()
+{
+    return currentPosition->value;
+}
---
+++ tape_deque.hpp
@@ -0,0 +1,29 @@
+#ifndef TAPE_DEQUE_H
+#define TAPE_DEQUE_H
+
+struct node_t
+{
+    bool value;
+    struct node_t* left;
+    struct node_t* right;
+    
+    node_t(bool v, struct node_t* l, struct node_t* r) : value(v), left(l), right(r) {}
+};
+typedef struct node_t node_t;
+
+class TapeDeque
+{
+    public:
+    TapeDeque();
+    ~TapeDeque();
+    void moveRight(bool write);
+    void moveLeft(bool write);
+    bool currentValue();
+    int score();
+    
+    private:
+    node_t* head;
+    node_t* currentPosition;
+};
+
+#endif
